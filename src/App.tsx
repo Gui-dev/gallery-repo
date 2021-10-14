f8982d0e13feeb00b65f318c89b60c55062bd9a8
Gui-dev <adrianon10@gmail.com>
2021-10-14 19:43:20 -0300

    feat: add helpers functions handleSubmit and handleDeletePhoto

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { FormEvent, useEffect, useState } from 'react'
 
-import { getAll } from './services/photo'
+import { getAllPhotos, insertPhoto, removePhoto } from './services/photo'
 
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 import { Load } from './components/Load'
-import { Container, Form, InputGroup, InputFile, File, Button } from './styles/Home'
+import { Container, Form, InputGroup, InputFile, File, Button, Warning, PhotoContainer } from './styles/Home'
+import { PhotoItem } from './components/PhotoItem'
 
 type PhotoProps = {
   name: string
@@ -15,24 +16,23 @@ type PhotoProps = {
 function App () {
   const [file, setFile] = useState('')
   const [loading, setLoading] = useState(false)
+  const [uploading, setUploading] = useState(false)
   const [photos, setPhotos] = useState<PhotoProps[]>([])
 
   useEffect(() => {
-    const getPhotos = async () => {
-      try {
-        setLoading(true)
-        const result = await getAll()
-        setPhotos(result)
-        setFile('')
-      } catch (error) {
-        console.log(error)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    getPhotos()
+    loadPhotos()
   }, [])
+
+  const loadPhotos = async () => {
+    setLoading(true)
+    getAllPhotos()
+      .then(response => {
+        setPhotos(response)
+        setFile('')
+      })
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+  }
 
   const handleImageChange = (event: FormEvent) => {
     const target = event.target as HTMLInputElement
@@ -42,7 +42,35 @@ function App () {
     }
   }
 
-  console.log(photos)
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const file = formData.get('image') as File
+
+    if (file.size === 0) {
+      console.log('Selecione uma imagem')
+      return
+    }
+
+    if (file && file.size > 0) {
+      try {
+        setUploading(true)
+        const newPhoto = await insertPhoto(file)
+        setPhotos([...photos, newPhoto])
+      } catch (error) {
+        const err = error as Error
+        console.log(err.message)
+      } finally {
+        setUploading(false)
+        setFile('')
+      }
+    }
+  }
+
+  const handleDeletePhoto = async (photoName: string) => {
+    await removePhoto(photoName)
+    await loadPhotos()
+  }
 
   return (
     <>
@@ -55,11 +83,12 @@ function App () {
               <Load />
               )
             : (
-                <Form>
+                <Form onSubmit={ handleSubmit }>
                   <InputGroup>
                     <label htmlFor="inpuFile">
                       <InputFile
                         type="file"
+                        name="image"
                         id="inpuFile"
                         onChange={ handleImageChange }
                       />
@@ -70,7 +99,7 @@ function App () {
                   </InputGroup>
 
                   <Button>
-                    Enviar
+                    { uploading ? (<Load height={42} width={42}/>) : 'Enviar' }
                   </Button>
                 </Form>
               )
@@ -82,6 +111,25 @@ function App () {
             <img src={file} alt="Preview Image"/>
           </div>
         ) }
+
+        { !loading && photos.length === 0 && (
+          <Warning>
+            <span>Você ainda não tem fotos cadastradas</span>
+          </Warning>
+        ) }
+
+        <PhotoContainer>
+        { !loading && photos.map(photo => {
+          return (
+              <PhotoItem
+                key={ photo.name }
+                photo={ photo }
+                onRemovePhoto={ handleDeletePhoto }
+              />
+          )
+        })
+        }
+        </PhotoContainer>
 
       </Container>
     </>
